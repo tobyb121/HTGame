6d003e955b8812a90ce10f2f1a902402676b0f93
tobyb121 <tobyb121@hotmail.co.uk>
2014-05-14 20:54:22 +0100

    Added Hemis The Troll

---
+++ src/Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        mainCamera = Camera.main;
+	}
+
+    private Camera mainCamera;
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 cameraDirection=transform.position-mainCamera.transform.position;
+        cameraDirection.y= 0;
+        Quaternion q = transform.rotation;
+        q.SetLookRotation(cameraDirection);
+        transform.rotation = q;
+    }
+}
