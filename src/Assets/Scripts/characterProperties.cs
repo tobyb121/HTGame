c0f904e0f68435cd29fee54dea50633d63aab404
hemisthetroll <nicko54@hotmail.co.uk>
2014-05-16 17:45:34 +0100

    Hello toby you are my friend

---
+++ src/Assets/Scripts/characterProperties.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using BloodyMuns;
+
+public class characterProperties : MonoBehaviour {
+
+	public Character character = new Character();
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		character.Position = transform.position;
+		character.Rotation = transform.rotation;
+		character.Velocity = rigidbody.velocity;
+	}
+}
--- src/Assets/Scripts/dataFlowManager.cs
+++ src/Assets/Scripts/dataFlowManager.cs
@@ -3,64 +3,125 @@ using System.Collections;
 using System.Threading;
 using System.Net.Sockets;
 using System.Net;
+using BloodyMuns;
+using System.IO;
 
 public class dataFlowManager : MonoBehaviour
 {
+	UdpClient sBroadcast;
+	IPAddress tcpHost;
+	ushort tcpPort;
+	ushort udpPort;
+	Socket tcp;
+	Socket udp;
+	string serverName;
+	string playerName="Mugabe";
+	public int bcPort = 5433;
+	public characterProperties characterProperties;
+	public bool connected;
 
-		// Use this for initialization
-		void Start ()
-		{
-				Thread thread = new Thread (new ThreadStart (threadStart));
-				thread.Start ();
+	// Use this for initialization
+	void Start ()
+	{
+		Thread thread = new Thread (new ThreadStart (threadStart));
+		thread.Start ();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (connected) {
+			MemoryStream sendStream=new MemoryStream();
+			characterProperties.character.writeCharacter(sendStream);
+			udp.Send(sendStream);
 		}
+
+	}
 	
-		// Update is called once per frame
-		void Update ()
-		{
-	
-		}
+	void threadStart ()
+	{
+		byte[] buffer = new byte[1024];
+		byte[] heartBeatResponse = new byte[] {0x02,0xFF};
 
-		UdpClient sBroadcast;
-		IPAddress tcpHost;
-		ushort tcpPort;
-		public int bcPort = 5433;
+		IPEndPoint bcAddress = captureBcAddress (bcPort);
+		tcp = initialiseTCP (bcAddress);
+		udp = initialiseUDP ();
 
-		void threadStart ()
-		{
-				byte[] buffer = new byte[64];
-				byte[] sendBuffer = new byte[] {0x02,0xFF};
+		int n=0;
+		while (true) {
+			int received = tcp.Receive (buffer);
+			print(n++);
 
-				IPEndPoint bcAddress = captureBcAddress (bcPort);
-				Socket s = initialiseTCP (bcAddress);
+			switch (buffer [1]) {
+			case 0xFE:
+				tcp.Send (heartBeatResponse);
+				print ("Ping");
+				break;
 
-				while (true) {
-						int received = s.Receive (buffer);
-						s.Send (sendBuffer);
-				}
-		}
+			case 0x10:
+				//initialse character ID
+				MemoryStream receivedStream = new MemoryStream(buffer,2,buffer.Length-2);
+				BinaryReader receivedBinary = new BinaryReader(receivedStream);
+				int charID = receivedBinary.ReadInt32();
+				characterProperties.character.ID=charID;
 
-		IPEndPoint captureBcAddress (int bcPort)
-		{
-				sBroadcast = new UdpClient (bcPort);
+				MemoryStream sendStream = new MemoryStream();
+				BinaryWriter sendBinary = new BinaryWriter(sendStream);
+				sendBinary.Write((byte)0x02);
+				sendBinary.Write((byte)0x20);
+				sendBinary.Write (playerName);
+				tcp.Send(sendStream.ToArray());
+				connected=true;
+				break;
+
+			case 0x11:
+				//new character
+				break;
 		
-				IPEndPoint capture = new IPEndPoint (IPAddress.Any, 0);
-				EndPoint captureRemote = (EndPoint)capture;
-		
-				byte[] bcByte = sBroadcast.Receive (ref capture);
-		
-				tcpHost = capture.Address;
-		
-				tcpPort |= bcByte [0];
-				tcpPort |= (ushort)(((ushort)bcByte [1]) << 8);
+			case 0x12:
+				//character update
+				break;
 
-				return new IPEndPoint (tcpHost, tcpPort);
-		}
 
-		Socket initialiseTCP (IPEndPoint bcAddress)
-		{
-				Socket s = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-				s.Connect (bcAddress);
-				return s;
+
+			}
+				
 		}
-	
+	}
+
+	IPEndPoint captureBcAddress (int bcPort)
+	{
+		sBroadcast = new UdpClient (bcPort);
+
+		IPEndPoint capture = new IPEndPoint (IPAddress.Any, 0);
+		EndPoint captureRemote = (EndPoint)capture;
+
+		MemoryStream receivedStream = new MemoryStream (sBroadcast.Receive (ref capture));
+		BinaryReader receivedBinary = new BinaryReader (receivedStream);
+		//byte[] bcByte = sBroadcast.Receive (ref capture);
+
+		tcpHost = capture.Address;
+
+		tcpPort = receivedBinary.ReadUInt16 ();
+		udpPort = receivedBinary.ReadUInt16 ();
+		serverName = receivedBinary.ReadString ();
+
+		return new IPEndPoint (tcpHost, tcpPort);
+	}
+
+	Socket initialiseTCP (IPEndPoint bcAddress)
+	{
+		Socket tcp = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+		tcp.Connect (bcAddress);
+		return tcp;
+	}
+
+	Socket initialiseUDP ()
+	{
+		Socket sUDP = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Udp);
+		sUDP.Bind(new IPEndPoint(IPAddress.Any,udpPort));
+		return sUDP;
+	}
+
 }
+
