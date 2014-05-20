8b24462b9163de15a1e7003cb2e90b343ca41277
tobyb121 <tobyb121@hotmail.co.uk>
2014-05-20 19:12:08 +0100

    Many Many Changes

---
+++ src/Assets/bulletMove.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletMove : MonoBehaviour {
+    void Start () {
+        rigidbody.velocity = 200 * transform.forward;
+        previousPosition = rigidbody.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private Vector3 previousPosition;
+
+    public LayerMask layerMask;
+
+    void FixedUpdate()
+    {
+        Vector3 movementThisStep =rigidbody.position - previousPosition;
+        float movementSqrMagnitude = movementThisStep.sqrMagnitude;
+
+        if (movementSqrMagnitude > sqrMinimumExtent)
+        {
+            float movementMagnitude = Mathf.Sqrt(movementSqrMagnitude);
+            RaycastHit hitInfo;
+
+            //check for obstructions we might have missed 
+            if (Physics.Raycast(previousPosition, movementThisStep, out hitInfo, movementMagnitude, layerMask.value))
+                myRigidbody.position = hitInfo.point - (movementThisStep / movementMagnitude) * partialExtent;
+        }
+
+        previousPosition = myRigidbody.position;
+    }
+
+}
---
+++ src/Assets/fireBullet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fireBullet : MonoBehaviour {
+
+    public Transform forward;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public GameObject bulletPrefab;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = (GameObject)GameObject.Instantiate(bulletPrefab);
+            bullet.transform.rotation = forward.rotation;
+            bullet.transform.position = forward.position;
+        }
+	}
+}
